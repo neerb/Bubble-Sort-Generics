43ff692454640d1b197b8d973ddb0ff8df8f8720
Nathan <31359612+neerb@users.noreply.github.com>
2019-07-13 15:56:47 -0500

    Add files via upload

---
+++ Bubble Sort Generics/Bubble Sort Generics/BubbleSort.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BubbleSort<T> where T : IComparable<T>
+{
+    T[] array { get; set; }
+    T[] returntoOrginalArray;
+
+    public BubbleSort(T[] arraytoSort)
+    {
+        array = arraytoSort;
+        returntoOrginalArray = arraytoSort;
+    }
+
+    int tracker = 0;
+    T current;
+    int totalExchanges;
+    int totalPasses;
+
+    public void Sort()
+    {
+        while(tracker < array.Length - 1)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                current = array[i];
+                if (array[i].CompareTo(array[i + 1]) > 0)
+                {
+                    array[i] = array[i + 1];
+                    array[i + 1] = current;
+                    tracker = 0;
+                    totalExchanges++;
+                }
+                else
+                    tracker++;
+            }
+            totalPasses++;
+        }
+
+        Console.WriteLine(">> Sorted Array: ");
+        printArray();
+    }
+    
+
+    private void printArray()
+    {
+        foreach (T variable in array)
+        {
+            Console.Write(String.Concat(variable.ToString(), ", "));
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(">> Total Exchanges: " + totalExchanges.ToString());
+        Console.WriteLine(">> Total Passes: " + totalPasses);
+
+        tracker = 0;
+        totalExchanges = 0;
+        totalPasses = 0;
+
+        array = returntoOrginalArray;
+
+        Console.WriteLine("-");
+    }
+}
+
---
+++ Bubble Sort Generics/Bubble Sort Generics/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_Sort_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 101, 102, 100, 100, 110, 109, 109, 108, 109 };
+            double[] doubleArray = new double[] { 23, 34, 26, 37, 25, 16, 28 };
+            string[] stringArray = new string[] {"z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a" };
+            byte[] byteArray = new byte[20];
+            bool[] boolArray = new bool[20];
+
+            Random rand = new Random();
+
+            //Initialization of byteArray
+            for(int i = 0; i < byteArray.Length; i++)
+            {
+                byteArray[i] = (byte)rand.Next(0, 256);
+            }
+
+            //Initialization of boolArray
+            for (int i = 0; i < boolArray.Length; i++)
+            {
+                if (rand.Next(0, 2) == 0)
+                    boolArray[i] = false;
+                else
+                    boolArray[i] = true;
+            }
+
+            BubbleSort<int> intSorter = new BubbleSort<int>(array);
+            BubbleSort<string> stringSorter = new BubbleSort<string>(stringArray);
+            BubbleSort<byte> byteSorter = new BubbleSort<byte>(byteArray);
+            BubbleSort<bool> boolSorter = new BubbleSort<bool>(boolArray);
+            BubbleSort<double> doubleSorter = new BubbleSort<double>(doubleArray);
+
+            Console.WriteLine(">> Array for sorting:");
+
+            //For any array defined above
+            foreach(var n in doubleArray)
+            {
+                Console.Write(String.Concat(n.ToString(), ", "));
+            }
+
+            Console.WriteLine();
+
+            intSorter.Sort();
+            doubleSorter.Sort();
+            stringSorter.Sort();
+            byteSorter.Sort();
+            boolSorter.Sort();
+
+            Console.ReadKey();
+        }
+    }
+}
